6ce0ae9509f1d84d1e55374e41e3bb0c1e09d91d
raphael rezzieri <rezzieri@gmail.com>
2020-07-07 09:58:41 -0400

    list all appointments

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ routes.put('/users', UserController.update);
 routes.get('/users/providers', UserController.allProviders);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
